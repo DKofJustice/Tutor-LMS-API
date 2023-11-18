3a28a78d4c68f1650d263ff376b403de823df83b
Alex Benta <alexbenta20@gmail.com>
2023-11-18 21:09:04 +0200

    Nodemon fix

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '2003nuva',
-    database: 'tutorlms'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 })
 
 // Register user in the Database
